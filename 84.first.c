131fe562d0e687960c64dc76825641526a364f0b
“vikrantsingh2105” <2025221009.vikrant@ug.sharda.ac.in>
2026-02-15 16:54:17 +0530

    Auto commit by Python script

--- 84.first.c
+++ 84.first.c
@@ -9,6 +9,6 @@ int main(){
     struct book b1,b2,b3;
 
     printf("enetr name,price & no of pages of 3 books\n");
-    scanf(" %c %f %d",&b1.name)
+    scanf(" %c %f %d",&b1.name,&b1.price)
     return 0;
 }
