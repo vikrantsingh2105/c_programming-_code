c334492838602acfcd055c842d2788bf3f4aa024
“vikrantsingh2105” <2025221009.vikrant@ug.sharda.ac.in>
2026-02-15 17:19:38 +0530

    Auto commit by Python script

--- 84.first.c
+++ 84.first.c
@@ -9,12 +9,12 @@ int main(){
     struct book b1,b2,b3;
 
     printf("enter name,price & no of pages of 3 books\n");
-    scanf(" %c %f %d",&b1.name,&b1.price,&b1.pages);
-    scanf(" %c %f %d",&b2.name,&b2.price,&b2.pages);
-    scanf(" %c %f %d",&b3.name,&b3.price,&b3.pages);
+    scanf(" %s %f %d",b1.name,&b1.price,&b1.pages);
+    scanf(" %s %f %d",b2.name,&b2.price,&b2.pages);
+    scanf(" %s %f %d",b3.name,&b3.price,&b3.pages);
     printf("and this is what u enetered\n");
-    printf(" %c %f %d\n",b1.name,b1.price,b1.pages);
-    printf(" %c %f %d\n",b2.name,b2.price,b2.pages);
-    printf(" %c %f %d\n",b3.name,b3.price,b3.pages);
+    printf(" %s %f %d\n",b1.name,b1.price,b1.pages);
+    printf(" %s %f %d\n",b2.name,b2.price,b2.pages);
+    printf(" %s %f %d\n",b3.name,b3.price,b3.pages);
     return 0;
 }
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+repo_path = "/Users/vikrantsingh/hello.c"
