5fe14d8594a650a1a24b4c90a1a1b1287640dc9a
“vikrantsingh2105” <2025221009.vikrant@ug.sharda.ac.in>
2026-02-15 17:01:02 +0530

    Auto commit by Python script

--- 84.first.c
+++ 84.first.c
@@ -11,8 +11,7 @@ int main(){
     printf("enetr name,price & no of pages of 3 books\n");
     scanf(" %c %f %d",&b1.name,&b1.price,&b1.pages);
     scanf(" %c %f %d",&b2.name,&b2.price,&b2.pages);
-    scanf(" %c %f %d",&b3.name,&b3.price,&b3.pages);
     printf("and thisis what u enetered\n");
-    
+    scanf(" %c %f %d",&b3.name,&b3.price,&b3.pages);
     return 0;
 }
