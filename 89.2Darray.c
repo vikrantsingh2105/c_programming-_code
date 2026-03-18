12cae693fe5331e17df90369d0fa823731124137
“vikrantsingh2105” <2025221009.vikrant@ug.sharda.ac.in>
2026-03-18 20:44:35 +0530

    Auto commit by Python script

---
+++ 89.2Darray.c
@@ -0,0 +1,25 @@
+//program to write table given by user in 2d array
+#include<stdio.h>
+
+int main(){
+    int a[3][10];
+    int mul[]={2,3,7};
+
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<10;j++)
+        {
+            a[i][j]= mul[i]*(j+1);
+        }
+    }
+
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<10;j++)
+        {
+            printf("%d x %d = %d\n",mul[i],(j+1),a[i][j]);
+        }
+        printf("\n");        
+    }
+    return 0;
+}
